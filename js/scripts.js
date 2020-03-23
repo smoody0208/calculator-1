5d4f47bb2cd2f3be2037e91902029a8ecdd6058f
Mark Gridley <mark_gridley@yahoo.com>
2020-03-23 10:11:54 -0700

    add initial calculator in js file coauthored by Ethan Firpo <efirpo@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
-var add = function(number1, number2) {
-  return number1 + number2;
+var add = function(num1, num2) {
+  return num1 + num2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter another number:"));
+var result = add(num1, num2);
+alert(add(num1, num2));
 
-alert(add(number1, number2));
+var subtract = function(num1, num2) {
+  return num1 - num2;
+};
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter another number:"));
+var result = subtract(num1, num2);
+alert(subtract(num1, num2));
+
+var multiply = function(num1,num2) {
+  return num1 * num2;
+};
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter another number:"));
+var result = multiply(num1,num2);
+alert(multiply(num1,num2));
+
+var divide = function(num1,num2) {
+  return num1 / num2;
+};
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter another number:"));
+var result = divide(num1,num2);
+alert(divide(num1,num2));
