9a8c163b3dc1f7129c06850120e1dcad0b25fe83
Ethan Firpo <efirpo@gmail.com>
2020-03-25 11:28:21 -0700

    add functionality to multiplication, division, subtraction form
    coauthored by Spencer Moody <spencer.moody@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -16,9 +16,28 @@ var divide = function(number1, number2) {
 
 $(document).ready(function() {
   $("form#add").submit(function(event) {
-  event.preventDefault();
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
-  alert(add(number1, number2));
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    alert(subtract(number1, number2));
+    });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mult1").val());
+    var number2 = parseInt($("#mult2").val());
+    alert(multiply(number1, number2));
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divi1").val());
+    var number2 = parseInt($("#divi2").val());
+    alert(divide(number1, number2));
+  })
   });
 });
+
